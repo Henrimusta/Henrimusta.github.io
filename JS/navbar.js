35d83775eb3d541721c0136679bc6446c21c6cbc
Henrimusta <mustakangas.henri@gmail.com>
2023-10-20 15:11:29 +0300

    Final
    Still needs new readme

--- JS/navbar.js
+++ JS/navbar.js
@@ -12,8 +12,8 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("activate");
-    navMenu.classList.toggle("activate");
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
