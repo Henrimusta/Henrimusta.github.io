7179d2e0b391f1771a509aef2d6f6b05ea46c53e
Henrimusta <mustakangas.henri@gmail.com>
2023-10-20 13:02:43 +0300

    uutta

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,9 +1,22 @@
 //Navbar to stick to top of page on scroll
-window.onscroll = function () {
+/*window.onscroll = function () {
     var navbar = document.getElementById("navbar");
     if (window.scrollY > 0) {
         navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
-};
+};*/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("activate");
+    navMenu.classList.toggle("activate");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
